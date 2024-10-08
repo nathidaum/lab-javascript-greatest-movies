4a880696f308ca594ecfeee65b5e24c331344359
nathidaum <nathidaum@gmail.com>
2024-10-08 18:17:57 +0200

    Finished first 4 iterations

--- src/movies.js
+++ src/movies.js
@@ -1,16 +1,49 @@
 // Iteration 1: All directors? - Get the array of all directors.
 // _Bonus_: It seems some of the directors had directed multiple movies so they will pop up multiple times in the array of directors.
 // How could you "clean" a bit this array and make it unified (without duplicates)?
-function getAllDirectors(moviesArray) {}
+function getAllDirectors(moviesArray) {
+    const directorsArr = moviesArray
+    .map((movieObj) => movieObj.director)
+    // .filter((movieObj, index, arr) => {})
+    return directorsArr;
+}
 
 // Iteration 2: Steven Spielberg. The best? - How many drama movies did STEVEN SPIELBERG direct?
-function howManyMovies(moviesArray) {}
+function howManyMovies(moviesArray) {
+    const stevenSpielbergMoviesArr = moviesArray.filter((movieObj) => movieObj.director === "Steven Spielberg" && movieObj.genre.includes("Drama")); 
+    return stevenSpielbergMoviesArr.length;
+}
 
 // Iteration 3: All scores average - Get the average of all scores with 2 decimals
-function scoresAverage(moviesArray) {}
+function scoresAverage(moviesArray) {
+    if(moviesArray.length === 0) {
+        return 0
+    }
+    
+    const totalScore = moviesArray.reduce((acc, movieObj) => {
+       if(movieObj.score === undefined) {
+        return acc
+       } else { 
+        return acc + movieObj.score
+    }
+    }, 0)
+    
+    return Math.round(totalScore / moviesArray.length * 100) / 100;
+};
 
 // Iteration 4: Drama movies - Get the average of Drama Movies
-function dramaMoviesScore(moviesArray) {}
+function dramaMoviesScore(moviesArray) {
+    
+    const dramasArr = moviesArray.filter((movieObj) => movieObj.genre.includes("Drama")); 
+    if(dramasArr.length === 0) {
+        return 0
+    };
+    
+    const totalDramaScore = dramasArr.reduce((acc, movieObj) => {
+       return acc + movieObj.score
+    }, 0)
+    return Math.round(totalDramaScore / dramasArr.length * 100) / 100;
+};
 
 // Iteration 5: Ordering by year - Order by year, ascending (in growing order)
 function orderByYear(moviesArray) {}
